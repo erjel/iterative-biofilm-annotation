69fcf1c28647d97c9317822256e336fca98c16e0
Eric Jelli <eric.jelli@mpinb.mpg.de>
2022-09-09 09:55:11 +0200

    Use same patch size in prediction as in training and normalize per patch

--- iterative_biofilm_annotation/unet/predict.py
+++ iterative_biofilm_annotation/unet/predict.py
@@ -18,7 +18,8 @@ def predict(
     input_data_list: List[Path],
     output_dir: Path,
     context: Optional[Tuple[int]] = (16, 16, 16),
-    chunk_size: Optional[Tuple[int]] = (128, 256, 256),
+    chunk_size: Optional[Tuple[int]] = (48, 96, 96),
+    gpu_batch_size: Optional[int] = 1,
  ) -> None:
 
     model = SegModel(None, modelname, basedir=basedir)
@@ -32,7 +33,6 @@ def predict(
     for input_path in input_data_list:
 
         img = imread(input_path)
-        img = normalize(img, 1,99.8)
 
         chunk_size_ = tuple(min([s, c]) for s,c in zip(chunk_size, img.shape)) 
         chunk_size_ = tuple(
@@ -49,12 +49,24 @@ def predict(
 
         img_processed_tiled = np.empty(img.shape + (3,), dtype=np.float32)
 
-        for b in tqdm(blocks):
-            input = b.read(img, axes=axes)
-            
-            result = process(input)
+        # START https://python-forum.io/thread-12022.html
+        import itertools 
+        def chunker_longest(iterable, chunksize):
+            return itertools.zip_longest(*[iter(iterable)] * chunksize)
 
-            b.write(img_processed_tiled, b.crop_context(result, axes=axes))
+        gpu_cycles = math.ceil(len(blocks) / gpu_batch_size)
+
+        for block_list in tqdm(chunker_longest(blocks, gpu_batch_size), total=gpu_cycles):
+            input = [b.read(img, axes=axes) for b in block_list if b is not None]
+            input = [normalize(i, 1, 99.8) for i in input]
+            # TODO(erjel): Train UNets with SZYXC axes to benefit from batch inference
+            #input = np.array(input)
+            #result = process(input)
+
+            input = input[0]
+            result = [process(input)]
+
+            [b.write(img_processed_tiled, b.crop_context(r, axes=axes)) for r, b in zip(result, block_list) if b is not None]
 
         lbl_pred = label(img_processed_tiled[...,1] > 0.7)
 
